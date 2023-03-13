571a53e8b34b979ba9ab210da4b36b02eeb28450
Krishna Kumar <57484737+krishna469@users.noreply.github.com>
2023-03-13 10:38:31 +0530

    Edited Some Code

--- InterFace/SmartPhoneDemo.java
+++ InterFace/SmartPhoneDemo.java
@@ -1,17 +1,17 @@
 public class SmartPhoneDemo {
     public static void main(String[] args) {
-        // Create SmartPhone object
+        
         SmartPhone myPhone = new SmartPhone();
     
-        // Call phone methods
+        
         myPhone.call();
         myPhone.sms();
     
-        // Call camera methods
+        
         myPhone.click();
         myPhone.record();
     
-        // Call music player methods
+        
         myPhone.play();
         myPhone.stop();
       }

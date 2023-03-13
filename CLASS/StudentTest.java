7dc38fb8677228dd41a78ad756a0969b7b665415
Krishna Kumar <57484737+krishna469@users.noreply.github.com>
2023-03-13 10:07:59 +0530

    Some Enhancement in code

--- CLASS/StudentTest.java
+++ CLASS/StudentTest.java
@@ -5,7 +5,7 @@ public class StudentTest {
 		
 
         Student s1 = new Student();
-        s1.setRollNumber(1);
+        s1.setRollNumber(20223053);
         s1.setName("krishna kumar");
         s1.setCourse("MCA");
         s1.setMarks1(80);
@@ -13,35 +13,29 @@ public class StudentTest {
         s1.setMarks3(85);
 
         Student s2 = new Student();
-        s2.setRollNumber(2);
-        s2.setName("Bob");
-        s2.setCourse("CS101");
+        s2.setRollNumber(20223052);
+        s2.setName("Vivek");
+        s2.setCourse("MCA");
         s2.setMarks1(70);
         s2.setMarks2(75);
         s2.setMarks3(80);
 
         Student s3 = new Student();
-        s3.setRollNumber(3);
-        s3.setName("Charlie");
-        s3.setCourse("CS101");
+        s3.setRollNumber(302345);
+        s3.setName("Amit kumar");
+        s3.setCourse("B.Tech.");
         s3.setMarks1(60);
         s3.setMarks2(70);
         s3.setMarks3(65);
 
         Student s4 = new Student();
-        s4.setRollNumber(4);
-		s4.setName("Charlie");
-        s4.setCourse("CS101");
+        s4.setRollNumber(402345);
+		s4.setName("Raja Kumar");
+        s4.setCourse("B.Arch.");
         s4.setMarks1(60);
         s4.setMarks2(70);
         s4.setMarks3(65);
 
-
-
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-		System.out.println(s4);
 	
 	}
 

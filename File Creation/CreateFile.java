52ae3ae0b6cd9969ff078b3f965078260bc63f64
Krishna Kumar <57484737+krishna469@users.noreply.github.com>
2023-03-14 12:51:11 +0530

    this is first commit

---
+++ File Creation/CreateFile.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class CreateFile {
+    public static void main(String[] args) {
+
+        PrintWriter outputStream = null;
+        java.util.Scanner keyboard = new java.util.Scanner(System.in);
+        System.out.println("Enter three lines of text:");
+        String line = null;
+        int count;
+
+        try {
+            outputStream = new PrintWriter(new FileOutputStream("out.txt"));
+            for (count = 1; count <= 3; count++) {
+                line = keyboard.nextLine();
+                outputStream.println(count + " " + line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error while creating the file \n");
+        }
+        outputStream.close();
+        keyboard.close();
+        System.out.println("... written to out.txt.");
+    }
+}
---
+++ File Creation/Pattern.java
@@ -0,0 +1,42 @@
+
+public class Pattern {
+
+    public static void main(String[] args) {
+        
+        java.util.Scanner scan=new java.util.Scanner(System.in);
+
+        System.out.println("Enter the number to print the pattern :");
+        int n=scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        
+
+        
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
